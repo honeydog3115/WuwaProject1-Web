298a27391d020b6411871943a387f724cf2e8769
honeydog3115 <tjs91002256@gmail.com>
2026-08-19 08:50:13 +0900

    update: subStatValue에 get, set 메서드 추가 및 li 태그 생성을 동적으로 수정

--- component/resonatorecho/table/subStatValue.js
+++ component/resonatorecho/table/subStatValue.js
@@ -1,14 +1,35 @@
 class SubStatValue extends HTMLElement{
-    connectedCallback(){
-        const optionNumber = 5;
-        const options = Array(optionNumber).fill(0).map(() => `
-            <li></li>
+    optionNumber = 5;
+    options = Array(optionNumber).fill(0).map(() => `
+    <li></li>
+    `).join('');
+
+    #subStatValues = []
+
+    get subStatValues(){
+        return this.#subStatValues;
+    }
+
+    set subStatValues(data){
+        if (!Array.isArray(data)) return;
+
+        this.#subStatValues = data
+        this.options = this.#subStatValues.map((subStatValue)=>`
+            <li data-id=${subStatValue.id}>${subStatValue.value}</li>
         `).join('');
 
+        this.render()
+    }
+
+    connectedCallback(){
+        this.render()        
+    }
+
+    render(){
         this.innerHTML = `
             <div class="subStatValue">
                 <ul>
-                    ${options}
+                    ${this.options}
                 </ul>
             </div>
         `

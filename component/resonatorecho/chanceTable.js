dba7bd20e2ddd5111c7e9307882c79561b19aac0
honeydog3115 <tjs91002256@gmail.com>
2026-08-19 13:52:28 +0900

    add: 에코 획득 확률 표 컴포넌트 ChaceTable 생성

---
+++ component/resonatorecho/chanceTable.js
@@ -0,0 +1,25 @@
+class ChanceTable extends HTMLElement{
+    #rowNumber = 5
+
+    connectedCallback(){
+        this.render()
+    }
+
+    render(){
+        const content = Array.from({length: this.#rowNumber}, ()=>`
+            <chance-gauge></chance-gauge>
+            <chance-value></chance-value>
+        `).join('')
+        
+        this.innerHTML = `
+            <div>
+                <p>상옵</p><p>등장확률</p>
+            </div>
+            <div>
+                ${content}
+            </div>
+        `
+    }
+}
+
+customElements.define("chance-table", ChanceTable)

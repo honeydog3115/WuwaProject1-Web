d1bee956da47c9100644a3589d7587cf439469d5
honeydog3115 <tjs91002256@gmail.com>
2026-08-19 09:05:53 +0900

    update: MainStatName 컴포넌트에 set, get 추가

--- component/resonatorecho/table/mainStatName.js
+++ component/resonatorecho/table/mainStatName.js
@@ -1,16 +1,35 @@
 class MainStatName extends HTMLElement{
-    connectedCallback(){
-        const optionNumber = 5;
-        const options = Array(optionNumber).fill(0).map(() => `
-            <li></li>
+    optionNumber = 5;
+    options = Array(optionNumber).fill(0).map(() => `
+    <li></li>
+    `).join('');
+
+    #mainStatNames = []
+
+    get mainStatNames(){
+        return this.#mainStatNames;
+    }
+
+    set mainStatNames(data){
+        this.#mainStatNames = data
+        this.options = this.#mainStatNames.map((mainStatName)=>`
+            <li>${mainStatName}</li>
         `).join('');
-        
+
+        this.render()
+    }
+
+    connectedCallback(){
+        this.render()        
+    }
+
+    render(){
         this.innerHTML = `
-        <div class="mainStatName">
-            <ul>
-                ${options}
-            </ul>
-        </div>
+            <div class="subStatValue">
+                <ul>
+                    ${this.options}
+                </ul>
+            </div>
         `
     }
 }

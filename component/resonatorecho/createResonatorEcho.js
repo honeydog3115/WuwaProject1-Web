d70f7c251923422300cdb2ba270a85b7da9c75a4
honeydog3115 <tjs91002256@gmail.com>
2026-08-19 09:15:43 +0900

    add: 에코 생성 테이블과 삭제, OCR 버튼을 포함하는 CreateResonatorEcho 컴포넌트 생성

--- component/resonatorecho/createResonatorEcho.js
+++ component/resonatorecho/createResonatorEcho.js
@@ -1,3 +1,18 @@
 class CreateResonatorEcho extends HTMLElement{
-    
+    connectedCallback(){
+        this.render()
+    }
+    render(){
+        this.innerHTML = `
+            <div>
+                <button></button>
+                <button></button>
+            </div>
+            <div>
+                <resonatorecho-table></resonatorecho-table>
+            </div>
+        `
+    }
 }
+
+customElements.define("create-resonatorecho", CreateResonatorEcho)

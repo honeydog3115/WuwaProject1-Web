f5f57b031ad6368714583e3aa8e73354ed20cf71
honeydog3115 <tjs91002256@gmail.com>
2026-08-21 08:31:34 +0900

    update: ResonatorFilterBtn의 Js setter getter 방식에 setAttribute 방식 연결. HTML data-*속성을 사용하기 위해

--- component/resonator/choice/ResonatorFilterBtn.js
+++ component/resonator/choice/ResonatorFilterBtn.js
@@ -1,23 +1,35 @@
 class ResonatorFilterBtn extends HTMLElement{
-    #id = 0
-    #name = ""
-    #imagePath = ""
-
-    get filterInfo(){
-        return {id: this.#id, name: this.#name, imagePath: this.#imagePath}
+    static get observedAttributes(){
+        return ['data-target']
     }
 
-    set filterInfo({id, name, imagePath}){
-        this.#id = id
-        this.#name = name
-        this.#imagePath = imagePath
+    #filterInfo = {id: 0, name: "", imagePath: ""}
+
+    get filterInfo(){
+        return this.#filterInfo
+    }
+
+    set filterInfo(data){
+        this.#filterInfo = data || {}
+        const jsonString = JSON.stringify(this.#filterInfo)
+        // 다를 때만 setAttribute를 호출해서 setAttribute와 setter가 서로 무한 호출하는 것을 막음
+        if (this.getAttribute("data-target") !== jsonString)
+            this.setAttribute("data-target", jsonString)
+        this.render()
     }
 
     connectedCallback(){
         this.render()
     }
 
+    attributeChangedCallback(name, oldValue, newValue){
+        if(oldValue !== newValue && name == "data-target"){
+            this.filterInfo = JSON.parse(newValue || {})
+        }
+    }
+
     render(){
+        const imagePath = this.#filterInfo.imagePath
         this.innerHTML = `
             <div>
                 <button>

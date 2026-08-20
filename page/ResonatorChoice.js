673304635c15f5f2d0455d7b1547620fd4337ae8
honeydog3115 <tjs91002256@gmail.com>
2026-08-20 20:51:59 +0900

    add: 공명자 선택창 제작중...

--- page/ResonatorChoice.js
+++ page/ResonatorChoice.js
@@ -3,18 +3,22 @@ import "../component/resonator/choice/ResonatorFilterBtn.js"
 import "../component/SearchComponent.js"
 
 class ResonatorChoice extends HTMLAnchorElement{
+    #starNumber = 0
+    #attributeNumber = 0
+    #weaponNumber = 0
+    #resonatorNumber = 0
 
     render(){
-        const starFilter = Array(3).fill(0).map(()=>`
+        const starFilter = Array(starNumber).fill(0).map(()=>`
             <resonator-filter-btn></resonator-filter-btn>
         `).join('')
-        const attributeFileter = Array(7).fill(0).map(()=>`
+        const attributeFileter = Array(attributeNumber).fill(0).map(()=>`
             <resonator-filter-btn></resonator-filter-btn>
         `).join('')
-        const weaponFilter = Array(6).fill(0).map(()=>`
+        const weaponFilter = Array(weaponNumber).fill(0).map(()=>`
             <resonator-filter-btn></resonator-filter-btn>
         `).join('')
-        const cardList = Array(10).fill().map(()=>`
+        const cardList = Array(resonatorNumber).fill(0).map(()=>`
             <resonator-card></resonator-card>
         `).join('')
         this.innerHTML=`

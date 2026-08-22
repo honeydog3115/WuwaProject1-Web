02020c10d478cf8a830642d1ab4325bfd9ef7902
honeydog3115 <tjs91002256@gmail.com>
2026-08-22 15:33:47 +0900

    add: 좌측 프리셋 버튼인 presetbtn 컴포넌트 추가

---
+++ component/preset/PresetBtn.js
@@ -0,0 +1,36 @@
+class PresetBtn extends HTMLElement{
+    #bookmark = false
+    #bookmarkOn = "imagePath/On"
+    #bookmarkOff = "imagePath/Off"
+    #preset = {id: 0, name: ""}
+
+    get preset(){
+        return this.#preset
+    }
+
+    set preset({id: id, name: name}){
+        this.#preset.id = id ? id : 0 
+        this.#preset.name = name ? name : ""
+        this.render
+    }
+
+    connectedCallBack(){
+        this.render()
+    }
+
+    render() {
+        const bookmarkImage = this.#bookmark ? this.#bookmarkOn:this.#bookmarkOff
+        const presetName = this.#preset.name
+        this.innerHTML = `
+            <div>
+                <button>
+                    <span>
+                        ${presetName}
+                    </span>
+                    <img src="${bookmarkImage}">
+                </button>
+            </div>
+        `
+    }
+}
+customElements.define("preset-btn", PresetBtn)

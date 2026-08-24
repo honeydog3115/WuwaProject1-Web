ecf182629490ac04537c7b0183fc608a45a5e6e2
honeydog3115 <tjs91002256@gmail.com>
2026-08-24 19:22:24 +0900

    feat: 공명자 디테일을 가져오는 api 생성

--- api/resonatorApi.js
+++ api/resonatorApi.js
@@ -1,3 +1,4 @@
 import { httpClient } from "./apiClient.js";
 
 export const getResonators = async ()=>{return await httpClient.get("/resonator/1")}
+export const getResonatorDetail = async (id) => {return await httpClient.get(`/resonator/${id}`)}

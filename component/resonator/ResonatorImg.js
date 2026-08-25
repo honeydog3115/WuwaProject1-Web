33f224f84e1b915e31624108a722156ae78c8e8f
honeydog3115 <tjs91002256@gmail.com>
2026-08-25 20:31:10 +0900

    feat: 환경 변수를 이용해서 파일 경로를 잡도록 수정.
    - 환경 변수를 이용해 이미지 폴더 경로 추가.
    - 기본 이미지를 위한 경로 추가
    - 환경 변수와 기본 이미지를 조합해서 이미지 경로 생성

--- component/resonator/ResonatorImg.js
+++ component/resonator/ResonatorImg.js
@@ -1,5 +1,9 @@
 class ResonatorImg extends HTMLElement{
-    #DEFAULT_IMAGE = "image/character/unknown.webp"
+    #IMG_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL
+
+    #DEFAULT_CHARACTER_IMG = "character/unknown.webp"
+    #CHARACTER_IMG_PATH = this.#IMG_BASE_URL + this.#DEFAULT_CHARACTER_IMG
+
     connectedCallback(){
         this.render()
     }
@@ -7,7 +11,7 @@ class ResonatorImg extends HTMLElement{
     render() {
         this.innerHTML = `
             <div>
-                <img src="${this.#DEFAULT_IMAGE}" alt="선택한 공명자 이미지를 불러오는데 실패했습니다.">
+                <img src="${this.#CHARACTER_IMG_PATH}" alt="선택한 공명자 이미지를 불러오는데 실패했습니다.">
             </div>
         `
     }

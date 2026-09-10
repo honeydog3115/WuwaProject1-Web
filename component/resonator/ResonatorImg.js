167e801bf9ff0d9b12bed8c48134b96d690abbf0
honeydog3115 <tjs91002256@gmail.com>
2026-09-10 19:08:41 +0900

    modify: 공명자 에코 화면에서 공명자와 속성 이미지 출력 방식 변경
    - ResonatorImg 의 경로 참조 방식을 env.js가 생기면서 수정함.

--- component/resonator/ResonatorImg.js
+++ component/resonator/ResonatorImg.js
@@ -1,8 +1,7 @@
 import { html, render } from 'lit';
+import { ATTRIBUTE_IMAGE_URL, RESONATOR_IMAGE_URL } from '../../config/env';
 
 class ResonatorImg extends HTMLElement{
-    #imgBaseUrl = import.meta.env.VITE_IMAGE_BASE_URL;
-
     #property = {
         attributeImg : import.meta.env.VITE_DEFAULT_ATTRIBUTE_IMG,
         resonatorImg : import.meta.env.VITE_DEFAULT_RESONATOR_IMG,
@@ -20,12 +19,11 @@ class ResonatorImg extends HTMLElement{
 
     render() {
         const {attributeImg, resonatorImg, resonatorName} = this.#property
-        const imgBaseUrl = this.#imgBaseUrl
 
         const template = html`
             <div>
-                <img class="attribute" src="${imgBaseUrl}${attributeImg}" alt="속성 이미지를 불러오는데 실패했습니다.">
-                <img class="resonator" src="${imgBaseUrl}${resonatorImg}" alt="선택한 공명자 이미지를 불러오는데 실패했습니다.">
+                <img class="attribute" src="${ATTRIBUTE_IMAGE_URL}${attributeImg}" alt="속성 이미지를 불러오는데 실패했습니다.">
+                <img class="resonator" src="${RESONATOR_IMAGE_URL}${resonatorImg}" alt="선택한 공명자 이미지를 불러오는데 실패했습니다.">
                 <span>${resonatorName}</span>
             </div>
         `

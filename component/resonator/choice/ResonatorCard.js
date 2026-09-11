e43c7157758dedc6f89d6da36d692b1dcb44d598
honeydog3115 <tjs91002256@gmail.com>
2026-09-11 11:17:59 +0900

    refactor: 공명자 카드에서 속성, 무기를 할당하고 얻는 코드 제거
    - attribute, weapon get/set 제거

--- component/resonator/choice/ResonatorCard.js
+++ component/resonator/choice/ResonatorCard.js
@@ -1,54 +1,25 @@
 import { ATTRIBUTE_IMAGE_URL, RESONATOR_IMAGE_URL, WEAPON_IMAGE_URL } from "../../../config/env"
 
 class ResonatorCard extends HTMLElement{
-    #attribute = {}
-    #weapon = {}
     #resonator = {}
-    #rendering = false
 
-    get attribute(){
-        return this.#attribute
-    }
-
-    set attribute(data){
-        this.#attribute = data
-        this.#requestRender()
-    }
-    
-    get weapon(){
-        return this.#weapon
-    }
-    
-    set weapon(data){
-        this.#weapon = data
-        this.#requestRender()
-    }
-    
     get resonator(){
         return this.#resonator
     }
     
     set resonator(data){
         this.#resonator = data        
-        this.#requestRender()
+        this.render()
     }
 
-    #requestRender() {
-        if(this.#rendering) return;
-        this.#rendering = true
-        
-        queueMicrotask(()=>{
-            this.render()
-            this.#rendering = false
-        })
-    }
     connectedCallback(){
         this.render()
     }
 
     render(){
         const attribute = this.#resonator ? this.#resonator.attribute : undefined 
-        const weapon = this.#resonator ? this.#resonator.weapon : undefined 
+        const weapon = this.#resonator ? this.#resonator.weapon : undefined
+        
         this.innerHTML = `
             <div>
                 <img src="${ATTRIBUTE_IMAGE_URL}${attribute.imagePath}" alt="">

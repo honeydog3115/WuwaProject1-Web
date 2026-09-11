8546853352608e326fbad9ac2d57ec8d0b2c5793
honeydog3115 <tjs91002256@gmail.com>
2026-09-11 11:14:01 +0900

    feat: 공명자 카드에 필요한 이미지가 출력되도록 코드 추가
    - 이미지 경로를 추가
    - attribute, weaopn을 따로 받는게 아니라resonator에서 가져오도록 수정

--- component/resonator/choice/ResonatorCard.js
+++ component/resonator/choice/ResonatorCard.js
@@ -1,3 +1,5 @@
+import { ATTRIBUTE_IMAGE_URL, RESONATOR_IMAGE_URL, WEAPON_IMAGE_URL } from "../../../config/env"
+
 class ResonatorCard extends HTMLElement{
     #attribute = {}
     #weapon = {}
@@ -45,13 +47,16 @@ class ResonatorCard extends HTMLElement{
     }
 
     render(){
+        const attribute = this.#resonator ? this.#resonator.attribute : undefined 
+        const weapon = this.#resonator ? this.#resonator.weapon : undefined 
         this.innerHTML = `
-            <div class="width-10vw">
-                <img src="${this.#attribute.image}" alt="">
-                <img src="${this.#weapon.image}" alt="">
-                <img src="${this.#resonator.image}" alt="">
+            <div>
+                <img src="${ATTRIBUTE_IMAGE_URL}${attribute.imagePath}" alt="">
+                <img src="${WEAPON_IMAGE_URL}${weapon.imagePath}" alt="">
+                <img src="${RESONATOR_IMAGE_URL}${this.#resonator.imagePath}" alt="">
                 <span>${this.#resonator.name}</span>
             </div>
         `
     }
 }
+customElements.define("resonator-card", ResonatorCard)
